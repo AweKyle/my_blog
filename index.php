6cd041f945ba6d5365c98779d60005f8007aa805
Nikolay <kyle.voronin@gmail.com>
2014-07-07 12:23:29 +0400

    Точка входа

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'protected/controllers/SiteController.php';
+
+require 'protected/views/header.php';
+?>
+<div id="content">
+</div>
+<div id="result_div_id"></div>
+</body>
+</html>
